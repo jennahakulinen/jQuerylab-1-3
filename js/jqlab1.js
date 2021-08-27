f74dd4719b2d1b8597af197bd83bbe0277deff8b
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2021-08-27 10:07:12 +0300

    first commit

---
+++ js/jqlab1.js
@@ -0,0 +1,30 @@
+$('p:eq(1)').attr('id', 'jotain');
+$('#fp').remove();
+$('#fdiv').css(
+    {
+    backgroundColor:'grey',
+    color: 'white'
+    }
+);
+
+/*function sayHello() {
+    $('#jotain').after('<p>Hello user!</p>');
+}
+
+$('#nappi').click(sayHello);
+*/
+
+$('#nappi').click(function() {
+    $('#jotain').after('<p>Hello user!</p>');
+});
+
+$('#4p').hide();
+
+$('#sdiv').hover(
+	function () {
+        $('#4p').text('About to select a link...').show();
+    },
+    function () {
+        $('#4p').hide();
+    }
+)
